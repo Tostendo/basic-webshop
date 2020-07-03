5370a9e82647b96ab0aba2e8fc60c3c112fbe092
Thomas Ostendorf <thomas.ostendorf@orbitlabs.de>
2020-07-03 16:59:32 +0200

    Add sign in out page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import SignInOutPage from "./pages/sign-in-out/sign-in-out.component";
 
 import Header from "./components/header/header.component";
 
@@ -14,6 +15,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInOutPage} />
       </Switch>
     </div>
   );
---
+++ src/pages/sign-in-out/sign-in-out.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./sign-in-out.scss";
+
+import SignIn from "../../components/sign-in/sign-in.component";
+
+const SignInOutPage = () => (
+  <div className="sign-in-out">
+    <SignIn />
+  </div>
+);
+
+export default SignInOutPage;
