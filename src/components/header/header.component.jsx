2376ef510e55cc1f9c53d088963c3bc7db2f7ff5
Thomas Ostendorf <thomas.ostendorf@orbitlabs.de>
2020-07-06 10:21:15 +0200

    Add cart + redux functionality for cart open

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,11 +3,14 @@ import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 
+import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
+
 import { auth } from "../../firebase/firebase.utils";
 
 import "./header.styles.scss";
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden, toggleCart }) => (
   <div className="header">
     <Link to="/" className="logo-container">
       <Logo className="logo" />
@@ -28,12 +31,15 @@ const Header = ({ currentUser }) => (
           SIGN IN
         </Link>
       )}
+      <CartIcon />
     </div>
+    {hidden ? null : <CartDropdown />}
   </div>
 );
 
 const mapStateToProps = (state) => ({
   currentUser: state.user.currentUser,
+  hidden: state.cart.hidden,
 });
 
 export default connect(mapStateToProps)(Header);
---
+++ src/redux/cart/cart.actions.js
@@ -0,0 +1,4 @@
+import { CartActionTypes } from "./cart.types";
+export const toggleCart = () => ({
+  type: CartActionTypes.TOGGLE_CART,
+});
